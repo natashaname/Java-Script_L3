0d336583e6df20dc0c74800f3c202496ba2c873c
natashaname <69076612+natashaname@users.noreply.github.com>
2021-10-19 07:05:28 +0300

    Add files via upload

---
+++ js3.js
@@ -0,0 +1,49 @@
+// function findPositiveNumbers(){
+//   let arr = [5, 4, 3, -3, -10, -1, 8, -20, 0]
+//  for (let i of arr) {
+//       if (i>0)
+//       alert (i)
+//    }
+//  }
+//findPositiveNumbers()
+
+
+
+//function findPositiveNumbers() {
+// let arr = [5, 4, 3, -3, -10, -1, 8, -20, 0]
+//let arrNew = []
+// for (let i of arr) {
+//if (i>0) arrNew.push(i)
+//}
+// alert(arrNew)
+//}
+//findPositiveNumbers ()
+
+
+
+
+//const numbers = [5, 4, 3, -3, -10, -1, 8, -20, 0]
+//function findPositiveNumbers(arr) {
+//let newArr = []
+//for (let i of arr) {
+//if (i>0) newArr.push(i)
+//}
+//return newArr
+//}
+//const resultNewArr = findPositiveNumbers (numbers)
+//console.warn (resultNewArr)
+
+
+//let arr = [5, 4, 3, 8, 0]
+//function filterFor(arr,a) {
+//let newArr = []
+//for (let i of arr) {
+//   if (i>=a) newArr.push (i)
+//}
+// return newArr
+//}
+//const resultNewArr = filterFor(arr, a)
+//console.warn (resultNewArr)
+
+
+
